06770bb54422d71348c2471638ad4407821cb9c8
aviladevs <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>
2025-10-16 04:51:49 -0300

    Add project files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0057A4",
+        secondary: "#00AEEF",
+        metallic: "#C0C0C0",
+      },
+    },
+  },
+  plugins: [],
+};
